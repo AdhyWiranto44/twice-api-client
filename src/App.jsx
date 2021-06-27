c9625925526a4a89102af36921d642a1052acba3
adhywiranto44 <adhywiranto68@gmail.com>
2021-06-27 21:16:27 +0700

    Make members component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Jumbotron from './components/Jumbotron';
+import axios from 'axios';
 
 function App() {
+  let [members, setMembers] = useState("No data");
+
+  axios.get('http://twice-api.adhywiranto.xyz/api/members')
+  .then((response) => {
+    let data = response['data'];
+    setMembers(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
   return (
     <div>
       <Navbar />
       <Jumbotron />
+      <div className="container mt-3">
+        <div className="row">
+          <h3 className="fw-bold">Members</h3>
+          {members['data'] && members['data'].map((member) => {
+            return (
+                <div className="col-md-3 mb-3">
+                  <div className="card">
+                    <img src={member['img']} className="card-img-top" alt={member['name']} />
+                    <div className="card-body">
+                      <h5 className="card-title">{member['name']}</h5>
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" className="btn btn-primary">Detail</a>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -6,7 +6,7 @@ export default function Jumbotron() {
             <div className="row mt-4">
                 <div className="col-md-12 text-center">
                     <img src="./logo.png" alt="TWICE API" width="100" />
-                    <h1 className="display-3 mt-3 fw-bold">TWICE API</h1>
+                    <h1 className="display-3 mt-3 fw-bold">TWICE API Client</h1>
                 </div>
                 <div className="col-md-12 text-center">
                     <p className="text-secondary h5">Get TWICE data such as members' profile and album information</p>
